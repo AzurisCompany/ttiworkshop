40cf5c1bb492829c91d43d5a83c9e55906ad26ac
Alessandro Binhara <binhara@gmail.com>
2017-08-12 11:35:51 -0300

    Primeira tela

---
+++ src/FlightApp/FlightApp/FlightApp/App.xaml.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Xamarin.Forms;
+
+namespace FlightApp
+{
+    public partial class App : Application
+    {
+        public static IList<Models.FlightsModel> FlightsResults { get; set; }
+
+        public App()
+        {
+            InitializeComponent();
+
+            FlightsResults = new List<Models.FlightsModel>
+            {
+                new Models.FlightsModel(){
+                    OrigemDestino = "Riberirao preto (RAO) - Guarulhos (GRU)",
+                    PartidaChegada = "09:21 - 10:25 (01:04)",
+                    TipoVoo = "Voo Direto",
+                    Observacao = "Nao existem conexoes neste voo....",
+                    Valor = "BRL 112.49"
+                },
+                new Models.FlightsModel(){
+                    OrigemDestino = "Riberirao preto (RAO) - Guarulhos (GRU)",
+                    PartidaChegada = "09:21 - 10:25 (01:04)",
+                    TipoVoo = "Voo Direto",
+                    Observacao = "Nao existem conexoes neste voo....",
+                    Valor = "BRL 112.49"
+                },
+                new Models.FlightsModel(){
+                    OrigemDestino = "Riberirao preto (RAO) - Guarulhos (GRU)",
+                    PartidaChegada = "09:21 - 10:25 (01:04)",
+                    TipoVoo = "Voo Direto",
+                    Observacao = "Nao existem conexoes neste voo....",
+                    Valor = "BRL 112.49"
+                },
+                new Models.FlightsModel(){
+                    OrigemDestino = "Riberirao preto (RAO) - Guarulhos (GRU)",
+                    PartidaChegada = "09:21 - 10:25 (01:04)",
+                    TipoVoo = "Voo Direto",
+                    Observacao = "Nao existem conexoes neste voo....",
+                    Valor = "BRL 112.49"
+                }
+            };
+
+            MainPage = new FlightApp.FlightChoose();
+        }
+
+        protected override void OnStart()
+        {
+            // Handle when your app starts
+        }
+
+        protected override void OnSleep()
+        {
+            // Handle when your app sleeps
+        }
+
+        protected override void OnResume()
+        {
+            // Handle when your app resumes
+        }
+    }
+}
---
+++ src/FlightApp/FlightApp/FlightApp/Models/FlightsModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlightApp.Models
+{
+    public class FlightsModel
+    {
+        public string OrigemDestino { get; set; }
+
+        public string PartidaChegada { get; set; }
+
+        public string TipoVoo { get; set; }
+
+        public string Observacao { get;  set; }
+
+        public string Valor { get; set; }
+    }
+}
